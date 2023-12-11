d7105c2aada8de1d8862a0072de6908cfdae774c
Omar J <omarj819@gmail.com>
2023-12-11 11:37:51 +0200

    A

---
+++ Betty.c
@@ -0,0 +1,28 @@
+#include "holberton.h" /* Include necessary header files */
+
+/**
+ * main - Entry point of the shell program
+ *
+ * Return: Always 0 on success
+ */
+int main(void)
+{
+    /* Your main shell loop and logic go here */
+    return (0);
+}
+
+/**
+ * custom_function - Example of a custom function in the shell
+ *
+ * @param1: Description of the first parameter
+ * @param2: Description of the second parameter
+ *
+ * Return: Description of the return value
+ */
+int custom_function(int param1, char *param2)
+{
+    /* Function implementation goes here */
+    return (0);
+}
+
+/* Add other functions as needed, but keep the number of functions per file <= 5 */
