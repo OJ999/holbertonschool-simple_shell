2895ee3dc8dc63a44bc3b71a7483cac34a37fccf
Omar J <omarj819@gmail.com>
2023-12-12 16:27:41 +0200

    A

--- aux_help2.c
+++ aux_help2.c
@@ -1,40 +1,61 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-/**
- * aux_help - Help information for the builtin help.
- * Return: no return
- */
-void aux_help(void)
-{
-	char *help = "help: help [-dms] [pattern ...]\n";
+#define ALIAS_MAX 50
+#define COMMAND_MAX 100
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "\tDisplay information about builtin commands.\n ";
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "Displays brief summaries of builtin commands.\n";
-	write(STDOUT_FILENO, help, _strlen(help));
+typedef struct {
+    char name[50];
+    char value[50];
+} Alias;
+
+Alias aliases[ALIAS_MAX];
+int aliasCount = 0;
+
+void displayAliases() {
+    for (int i = 0; i < aliasCount; i++) {
+        printf("%s='%s'\n", aliases[i].name, aliases[i].value);
+    }
 }
-/**
- * aux_help_alias - Help information for the builtin alias.
- * Return: no return
- */
-void aux_help_alias(void)
-{
-	char *help = "alias: alias [-p] [name[=value]...]\n";
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "\tDefine or display aliases.\n ";
-	write(STDOUT_FILENO, help, _strlen(help));
+void setAlias(char *input) {
+    char *name = strtok(input, "=");
+    char *value = strtok(NULL, "");
+
+    if (name != NULL && value != NULL && aliasCount < ALIAS_MAX) {
+        strcpy(aliases[aliasCount].name, name);
+        strcpy(aliases[aliasCount].value, value);
+        aliasCount++;
+    } else {
+        fprintf(stderr, "Error: Invalid alias format\n");
+    }
 }
-/**
- * aux_help_cd - Help information for the builtin alias.
- * Return: no return
- */
-void aux_help_cd(void)
-{
-	char *help = "cd: cd [-L|[-P [-e]] [-@]] [dir]\n";
 
-	write(STDOUT_FILENO, help, _strlen(help));
-	help = "\tChange the shell working directory.\n ";
-	write(STDOUT_FILENO, help, _strlen(help));
+int main() {
+    char command[COMMAND_MAX];
+
+    while (1) {
+        printf("$ ");
+        if (fgets(command, COMMAND_MAX, stdin) == NULL) {
+            break; // End of file (Ctrl+D)
+        }
+
+        // Remove newline character
+        command[strcspn(command, "\n")] = '\0';
+
+        if (strncmp(command, "alias", 5) == 0) {
+            char *arguments = command + 5;
+            if (strlen(arguments) > 0) {
+                setAlias(arguments);
+            } else {
+                displayAliases();
+            }
+        } else {
+            // Handle other commands
+            printf("Executing: %s\n", command);
+        }
+    }
+
+    return 0;
 }
