68b5f8d0beb41815fd61feff6edd1ec5ba0eba56
Omar J <omarj819@gmail.com>
2023-12-13 20:31:03 +0200

    A

--- aux_help2.c
+++ aux_help2.c
@@ -1,40 +1,93 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-/**
- * aux_help - Help information for the builtin help.
- * Return: no return
- */
-void aux_help(void)
-{
-    char *help = "help: help [-dms] [pattern ...]\n";
+/* Assume _strlen() and other necessary functions are implemented */
 
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = "\tDisplay information about builtin commands.\n ";
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = "Displays brief summaries of builtin commands.\n";
-    write(STDOUT_FILENO, help, _strlen(help));
+/* Structure to store aliases */
+typedef struct Alias {
+    char *name;
+    char *value;
+} Alias;
+
+/* Function to print aliases */
+void print_aliases(Alias *aliases, int count) {
+    for (int i = 0; i < count; i++) {
+        printf("%s='%s'\n", aliases[i].name, aliases[i].value);
+    }
 }
-/**
- * aux_help_alias - Help information for the builtin alias.
- * Return: no return
- */
-void aux_help_alias(void)
-{
-    char *help = "alias: alias [-p] [name[=value]...]\n";
 
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = "\tDefine or display aliases.\n ";
-    write(STDOUT_FILENO, help, _strlen(help));
+/* Function to find an alias by name */
+int find_alias_index(Alias *aliases, int count, const char *name) {
+    for (int i = 0; i < count; i++) {
+        if (strcmp(aliases[i].name, name) == 0) {
+            return i;
+        }
+    }
+    return -1;
 }
-/**
- * aux_help_cd - Help information for the builtin alias.
- * Return: no return
- */
-void aux_help_cd(void)
-{
-    char *help = "cd: cd [-L|[-P [-e]] [-@]] [dir]\n";
 
-    write(STDOUT_FILENO, help, _strlen(help));
-    help = "\tChange the shell working directory.\n ";
-    write(STDOUT_FILENO, help, _strlen(help));
+/* Function to add or update an alias */
+void add_alias(Alias **aliases, int *count, const char *name, const char *value) {
+    int index = find_alias_index(*aliases, *count, name);
+    
+    if (index != -1) {
+        /* Update existing alias */
+        free((*aliases)[index].value);
+        (*aliases)[index].value = strdup(value);
+    } else {
+        /* Add new alias */
+        (*count)++;
+        *aliases = realloc(*aliases, (*count) * sizeof(Alias));
+        (*aliases)[*count - 1].name = strdup(name);
+        (*aliases)[*count - 1].value = strdup(value);
+    }
+}
+
+/* Function to handle the 'alias' command */
+void handle_alias(char *args) {
+    char *token;
+    char *saveptr;
+    int count = 0;
+    Alias *aliases = NULL;
+
+    token = strtok_r(args, " \t\n", &saveptr);
+
+    while (token != NULL) {
+        if (strcmp(token, "alias") == 0) {
+            /* Skip the initial 'alias' keyword */
+            token = strtok_r(NULL, " \t\n", &saveptr);
+            continue;
+        }
+
+        /* Check if the token contains '=' */
+        char *equal_sign = strchr(token, '=');
+
+        if (equal_sign != NULL) {
+            /* Token is in the form 'name=value' */
+            *equal_sign = '\0';
+            add_alias(&aliases, &count, token, equal_sign + 1);
+        } else {
+            /* Token is just an alias name */
+            int index = find_alias_index(aliases, count, token);
+            if (index != -1) {
+                printf("%s='%s'\n", aliases[index].name, aliases[index].value);
+            }
+        }
+
+        token = strtok_r(NULL, " \t\n", &saveptr);
+    }
+
+    /* Print all aliases if no specific aliases were requested */
+    if (count > 0 && aliases != NULL) {
+        print_aliases(aliases, count);
+    }
+
+    /* Free allocated memory */
+    for (int i = 0; i < count; i++) {
+        free(aliases[i].name);
+        free(aliases[i].value);
+    }
+    free(aliases);
 }
